c52917bdf87e0513e4f81883ae9058c0f222e196
Shashank Gaikwad <shashank.gaikwad@synerzip.com>
2019-09-02 23:41:13 +0530

    CreateRelationship Component converted into Functional Component

--- src/browser/modules/EditorInfo/CreateRelationship.jsx
+++ src/browser/modules/EditorInfo/CreateRelationship.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import {
   DrawerSection,
@@ -30,149 +30,142 @@ const IconButton = styled.button`
 /**
  * Component to Create New Relationship
  */
-export default class CreateRelationship extends Component {
-  componentDidMount () {
-    this.props.fetchSelectOptions('relationship', 'relationshipType')
-    this.props.fetchSelectOptions('relationship', 'label')
-  }
+export default function CreateRelationship (props) {
+  const [direction, setDirection] = useState(null)
+  const [selectedType, setSelectedType] = useState(null)
+  const [selectedLabel, setSelectedLabel] = useState(null)
+  const [selectedNode, setSelectedNode] = useState(null)
 
-  state = {
-    direction: null,
-    selectedType: null,
-    selectedLabel: null,
-    selectedNode: null
-  }
-
-  render () {
-    const { direction, selectedType, selectedLabel, selectedNode } = this.state
-    return (
-      <React.Fragment>
-        <DrawerSection>
-          <DrawerSectionBody>
-            <StyledTable>
-              <tbody>
-                <tr>
-                  <StyledKey> Direction:</StyledKey>
-                  <StyledValue>
-                    <FormControl
+  console.log('props received - ', props)
+  useEffect(
+    () => {
+      console.log('props inside useEffect - ', props)
+      props.fetchSelectOptions('relationship', 'relationshipType')
+      props.fetchSelectOptions('relationship', 'label')
+      selectedLabel ? props.fetchSelectOptions('Node', selectedLabel.value) : ''
+    },
+    [selectedLabel]
+  )
+  return (
+    <React.Fragment>
+      <DrawerSection>
+        <DrawerSectionBody>
+          <StyledTable>
+            <tbody>
+              <tr>
+                <StyledKey> Direction:</StyledKey>
+                <StyledValue>
+                  <FormControl
+                    style={{
+                      marginTop: 16,
+                      marginBottom: 16,
+                      minWidth: 120
+                    }}
+                    variant='outlined'
+                  >
+                    <Select
+                      name='datatype'
                       style={{
-                        marginTop: 16,
-                        marginBottom: 16,
-                        minWidth: 120
+                        background: '#fff',
+                        fontSize: '14px',
+                        textAlign: '-webkit-center',
+                        height: '34px',
+                        color: '#555',
+                        borderTop: '1px solid #ccc',
+                        borderRadius: '4px'
                       }}
-                      variant='outlined'
+                      value={direction}
+                      onChange={e => {
+                        setDirection(e.target.value)
+                      }}
                     >
-                      <Select
-                        name='datatype'
-                        style={{
-                          background: '#fff',
-                          fontSize: '14px',
-                          textAlign: '-webkit-center',
-                          height: '34px',
-                          color: '#555',
-                          borderTop: '1px solid #ccc',
-                          borderRadius: '4px'
-                        }}
-                        value={direction}
-                        onChange={e => {
-                          this.setState({ direction: e.target.value })
-                        }}
-                      >
-                        <MenuItem value='<----'>{'<---- (Incoming)'}</MenuItem>
-                        <MenuItem value='---->'>{'----> (Outgoing)'}</MenuItem>
-                      </Select>
-                    </FormControl>
-                  </StyledValue>
-                </tr>
-                <tr>
-                  <StyledKey>Type:</StyledKey>
-                  <StyledValue
-                    style={{ width: '100%' }}
-                    data-testid='user-details-username'
-                  >
-                    <CreatableSelect
-                      isClearable
-                      value={selectedType}
-                      onChange={selectedType => {
-                        this.setState({ selectedType })
-                      }}
-                      options={this.props.relationshipTypeList}
-                    />
-                  </StyledValue>
-                </tr>
-                <tr>
-                  <StyledKey>Label:</StyledKey>
-                  <StyledValue
-                    style={{ width: '100%' }}
-                    data-testid='user-details-username'
-                  >
-                    <CreatableSelect
-                      isClearable
-                      value={selectedLabel}
-                      onChange={selectedLabel => {
-                        this.setState({ selectedLabel }, () => {
-                          selectedLabel
-                            ? this.props.fetchSelectOptions(
-                              'Node',
-                              selectedLabel.value
-                            )
-                            : ''
-                        })
-                      }}
-                      options={this.props.labelList}
-                    />
-                  </StyledValue>
-                </tr>
-                <tr>
-                  <StyledKey>Node:</StyledKey>
-                  <StyledValue
-                    style={{ width: '100%' }}
-                    data-testid='user-details-username'
-                  >
-                    <CreatableSelect
-                      isClearable
-                      value={selectedNode}
-                      onChange={selectedNode => {
-                        this.setState({ selectedNode })
-                      }}
-                      options={this.props.nodeList}
-                    />
-                  </StyledValue>
-                </tr>
-              </tbody>
-            </StyledTable>
-            <ConfirmationButton
-              requestIcon={
-                <IconButton>
-                  <PlusIcon />
-                </IconButton>
-              }
-              cancelIcon={
-                <IconButton onClick={() => {}}>
-                  <CancelIcon />
-                </IconButton>
-              }
-              confirmIcon={<TickMarkIcon />}
-              onConfirmed={() => {
-                this.props.editEntityAction(
-                  {
-                    direction: this.state.direction,
-                    startNodeId: this.props.node.identity.toInt(),
-                    startNodeLabel: this.props.node.labels[0],
-                    endNodeId: this.state.selectedNode.value.identity.toInt(),
-                    endNodeLabel: this.state.selectedNode.value.labels[0],
-                    relationshipType: this.state.selectedType.value
-                  },
-                  'create',
-                  'relationship'
-                )
-              }}
-            />
-          </DrawerSectionBody>
-        </DrawerSection>
-      </React.Fragment>
-    )
-  }
+                      <MenuItem value='<----'>{'<---- (Incoming)'}</MenuItem>
+                      <MenuItem value='---->'>{'----> (Outgoing)'}</MenuItem>
+                    </Select>
+                  </FormControl>
+                </StyledValue>
+              </tr>
+              <tr>
+                <StyledKey>Type:</StyledKey>
+                <StyledValue
+                  style={{ width: '100%' }}
+                  data-testid='user-details-username'
+                >
+                  <CreatableSelect
+                    isClearable
+                    value={selectedType}
+                    onChange={selectedType => {
+                      setSelectedType(selectedType)
+                    }}
+                    options={props.relationshipTypeList}
+                  />
+                </StyledValue>
+              </tr>
+              <tr>
+                <StyledKey>Label:</StyledKey>
+                <StyledValue
+                  style={{ width: '100%' }}
+                  data-testid='user-details-username'
+                >
+                  <CreatableSelect
+                    isClearable
+                    value={selectedLabel}
+                    onChange={selectedLabel => {
+                      setSelectedLabel(selectedLabel)
+                    }}
+                    options={props.labelList}
+                  />
+                </StyledValue>
+              </tr>
+              <tr>
+                <StyledKey>Node:</StyledKey>
+                <StyledValue
+                  style={{ width: '100%' }}
+                  data-testid='user-details-username'
+                >
+                  <CreatableSelect
+                    isClearable
+                    value={selectedNode}
+                    onChange={selectedNode => {
+                      setSelectedNode(selectedNode)
+                    }}
+                    options={props.nodeList}
+                  />
+                </StyledValue>
+              </tr>
+            </tbody>
+          </StyledTable>
+          <ConfirmationButton
+            requestIcon={
+              <IconButton>
+                <PlusIcon />
+              </IconButton>
+            }
+            cancelIcon={
+              <IconButton onClick={() => {}}>
+                <CancelIcon />
+              </IconButton>
+            }
+            confirmIcon={<TickMarkIcon />}
+            onConfirmed={() => {
+              props.editEntityAction(
+                {
+                  direction: direction,
+                  startNodeId: props.node.identity.toInt(),
+                  startNodeLabel: props.node.labels[0],
+                  endNodeId: selectedNode.value.identity.toInt(),
+                  endNodeLabel: selectedNode.value.labels[0],
+                  relationshipType: selectedType.value
+                },
+                'create',
+                'relationship'
+              )
+            }}
+          />
+        </DrawerSectionBody>
+      </DrawerSection>
+    </React.Fragment>
+  )
 }
 
 CreateRelationship.propTypes = {
